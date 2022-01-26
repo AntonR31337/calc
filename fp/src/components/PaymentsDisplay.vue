249a2a05c862c6f03040f2e844a710b7060eee7b
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-01-26 17:53:39 +0300

    added several components to the project

---
+++ fp/src/components/PaymentsDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <div class="item" v-for="item in items" :key="item.id">
+      {{ item }} - {{ item.id }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- fp/src/components/money.vue
+++ fp/src/components/money.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="wrapper">
-    <h1>{{ name }}</h1>
-    <MyButton />
-    <MyButton />
+  <div class="home">
+    <header><h1>{{ name }}</h1></header>
+    <main>
+      <MyButton />
+      <AddPaymentForm @addNewPayment="addPayment" />
+      <PaymentsDisplay :items="PaymentsList" />
+    </main>
   </div>
 </template>
 
 <script>
 import MyButton from "./MyButton.vue";
+import PaymentsDisplay from "./PaymentsDisplay.vue"
+import AddPaymentForm from "./AddPaymentForm.vue"
 
 export default {
   name: "Money",
   components: {
     MyButton,
+    PaymentsDisplay,
+    AddPaymentForm
   },
   props: {
     msg: String,
@@ -20,8 +27,39 @@ export default {
   data() {
     return {
       name: "My personal costs",
+      PaymentsList: [],
     };
   },
+  methods: {
+    addPayment(data){
+      this.PaymentsList = [...this.PaymentsList, data]
+    },
+    fetchData(){
+      return [
+         {
+        id: 1,
+        date: '28.03.2020',
+        category: 'Food',
+        value: 169,
+      },
+      {
+        id: 2,
+        date: '24.03.2020',
+        category: 'Transport',
+        value: 360,
+      },
+      {
+        id: 3,
+        date: '24.03.2020',
+        category: 'Food',
+        value: 532,
+      },
+      ]
+    }
+  },
+  created(){
+    this.PaymentsList = this.fetchData()
+  }
 };
 </script>
 
